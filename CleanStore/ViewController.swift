a1426af6221cb2f9fdf216210beab88decbc8127
Raymond Law <ray@rayvinly.com>
2015-08-22 00:05:53 -0400

    Initial Commit

---
+++ CleanStore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CleanStore
+//
+//  Created by Raymond Law on 8/22/15.
+//  Copyright (c) 2015 Raymond Law. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
